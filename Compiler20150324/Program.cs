4e3b106e4f0be6308d6f806c5ac23179e054bea1
wanghesai <cocoyimasa@sina.cn>
2015-03-28 10:04:45 +0800

    init 2015_3_28
    init

---
+++ Compiler20150324/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if while语法：if while的条件不用括号括起来
+            //lambda语法:lambda x:int,y:int->int{let a=(a+b);}
+            string code = @"def a()
+                          {
+                              let a1=10.00001;
+                              let str=""aaaaaaaa"";
+                          }
+                          //eeeyeyeyeyeyeyeyeyeyeyeyeyyeyeyeyeyeyeyeyeyyeyeye
+                          def b(c,d){
+                          let d=90;
+                          //let a2=(-100)+200+d-20 + d*2-d/2+100*0.3*0.01*0.1*10*10*0.1*1*2/2;
+                          let a2=(-100)+d*2+100*0.3;
+                          let e =0;
+                          //let c = (lambda(e,f){let z=1;})(10,20);
+                          while a2>c {let a2=0.9;}
+                          if d<>0 or a2>c {
+                              let c=10;
+                          }
+                          let z=(-10.2);
+                          }
+                          ";
+            //            string code = @"def a()
+            //                            {
+            //                                 let c = (lambda(e,f){let z=1;})(10,20);
+            //                            }";
+//            string code = @"def a()
+//                            {
+//                                let c = (-100)+d*2+100*0.3;
+//                                return c;
+//                            }
+//                            a()
+//                            ";
+            LexicalAnalyzer.InitTokenRules();
+            List<Token> tokens = LexicalAnalyzer.Tokenizer(code);
+            foreach (var item in tokens)
+            {
+                //固定长度输出 string.PadLeft string.PadRight
+                Console.WriteLine(item.Type.ToString().PadRight(12, ' ') + ":   " + item.Value);
+            }
+            SyntacticalAnalyzer.tokens = tokens;
+            Node result = SyntacticalAnalyzer.ParseProgram();
+            if (result != null)
+            {
+                Console.WriteLine("语法分析完成！");
+                //Console.WriteLine(((FunctionStatement)result.children[0]).decl.funcName);
+                //Console.WriteLine(((FunctionStatement)result.children[0]).body[0].value.Value);
+            }
+            TypeChecker tc = new TypeChecker();
+            TypeChecker.self = tc;
+            Value value=tc.TypeCheck(result);
+            Console.WriteLine(value.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Compiler20150324/Scope.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class Scope
+    {
+        public Dictionary<string, Dictionary<string, object>> table =
+            new Dictionary<string, Dictionary<string, object>>();
+        public Scope parent;
+        public Scope()
+        {
+        }
+        public Scope(Scope p)
+        {
+            this.parent = p;
+        }
+        public Scope Copy()
+        {
+            Scope ret = new Scope();
+            foreach (string name in table.Keys)
+            {
+                Dictionary<string, object> props = new Dictionary<string, object>();
+                foreach (var p in table[name])
+                {
+                    props.Add(p.Key, p.Value);
+                }
+                ret.table.Add(name, props);
+            }
+            return ret;
+        }
+
+
+        public void putAll(Scope other)
+        {
+            foreach (string name in other.table.Keys)
+            {
+                Dictionary<string, object> props = new Dictionary<string, object>();
+                foreach (var p in other.table[name])
+                {
+                    props.Add(p.Key, p.Value);
+                }
+                table.Add(name, props);
+            }
+        }
+
+
+        public Value lookup(string name)
+        {
+            object v = lookupProperty(name, "value");
+            if (v == null)
+            {
+                return null;
+            }
+            else if (v is Value)
+            {
+                return (Value)v;
+            }
+            else
+            {
+                throw new CodeException(Token.NewToken(""), 
+                    "value is not a Value, shouldn't happen: " + v.ToString());
+            }
+        }
+
+
+        public Value lookupLocal(string name)
+        {
+            object v = lookupPropertyLocal(name, "value");
+            if (v == null)
+            {
+                return null;
+            }
+            else if (v is Value)
+            {
+                return (Value)v;
+            }
+            else
+            {
+                throw new CodeException(Token.NewToken(""), 
+                    "value is not a Value, shouldn't happen: " + v.ToString());
+            }
+        }
+
+
+        public Value lookupType(string name)
+        {
+            object v = lookupProperty(name, "type");
+            if (v == null)
+            {
+                return null;
+            }
+            else if (v is Value)
+            {
+                return (Value)v;
+            }
+            else
+            {
+                throw new CodeException(Token.NewToken(""), 
+                    "value is not a Value, shouldn't happen: " + v.ToString());
+            }
+        }
+
+
+        public Value lookupLocalType(string name)
+        {
+            object v = lookupPropertyLocal(name, "type");
+            if (v == null)
+            {
+                return null;
+            }
+            else if (v is Value)
+            {
+                return (Value)v;
+            }
+            else
+            {
+                throw new CodeException(Token.NewToken(""), 
+                    "value is not a Value, shouldn't happen: " + v.ToString());
+            }
+        }
+
+
+        public object lookupPropertyLocal(string name, string key)
+        {
+            if (table.ContainsKey(name))
+            {
+                Dictionary<string, object> item = table[name];
+                if(item.ContainsKey(key))
+                {
+                    return item[key];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public object lookupProperty(string name, string key)
+        {
+            object v = lookupPropertyLocal(name, key);
+            if (v != null)
+            {
+                return v;
+            }
+            else if (parent != null)
+            {
+                return parent.lookupProperty(name, key);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public Dictionary<string, object> lookupAllProps(string name)
+        {
+            if (table.ContainsKey(name))
+            {
+                return table[name];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public Scope findDefiningScope(string name)
+        {
+            if (table.ContainsKey(name))
+            {
+                return this;
+            }
+            else if (parent != null)
+            {
+                return parent.findDefiningScope(name);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Put(string name, string key, object value)
+        {
+            Dictionary<string, object> item = null;
+            if (table.ContainsKey(name))
+            {
+                item = table[name];
+            }
+            else
+            {
+                item=new Dictionary<string, object>();
+            }
+            item.Add(key, value);
+            table.Add(name, item);
+        }
+
+
+        public void PutProperties(string name, Dictionary<string, object> props)
+        {
+            Dictionary<string, object> item = null;
+            if (table.ContainsKey(name))
+            {
+                item = table[name];
+            }
+            else
+            {
+                item = new Dictionary<string, object>();
+            }
+            if(props !=null)
+            {
+                foreach(var p in props)
+                {
+                    item.Add(p.Key, p.Value);
+                }
+            }
+            table.Add(name, item);
+        }
+        public void putValue(string name, Value value)
+        {
+            Put(name, "value", value);
+        }
+        public void putType(String name, Value value)
+        {
+            Put(name, "type", value);
+        }
+        public static Scope initScope()
+        {
+            Scope init = new Scope();
+
+            init.putValue("+", new AddPrim());
+            init.putValue("-", new SubPrim());
+            init.putValue("*", new MulPrim());
+            init.putValue("/", new DivPrim());
+
+            init.putValue("<", new LtPrim());
+            init.putValue("<=", new LePrim());
+            init.putValue(">", new GtPrim());
+            init.putValue(">=", new GePrim());
+            init.putValue("=", new EqPrim());
+            init.putValue("and", new AndPrim());
+            init.putValue("or", new OrPrim());
+            init.putValue("not", new NotPrim());
+            init.putValue("<>", new NotEqPrim());
+
+            init.putValue("true", Value.BOOL);
+            init.putValue("false", Value.BOOL);
+
+            init.putValue("Int", Value.INT);
+            init.putValue("Bool", Value.BOOL);
+            init.putValue("String", Value.STRING);
+            init.putValue("Any", Value.ANY);
+
+            return init;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < table.Keys.Count;i++)
+            {
+                var item = table.ElementAt(i).Value;
+
+                for (int j = 0; j < item.Count;j++ )
+                {
+                    var val = item.ElementAt(j);
+                    if(j==item.Count-1)
+                    {
+                        sb.Append(val.Value.ToString());
+
+                    }
+                    else
+                    {
+                        sb.Append(val.Value.ToString() + "->");
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
